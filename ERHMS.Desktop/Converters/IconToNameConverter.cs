23b265054b4d0cca2d10d53a7a776a05b981d19a
Steven Williams <gev3@cdc.gov>
2020-07-31 13:07:05 -0400

    Add accessibility text for images

---
+++ ERHMS.Desktop/Converters/IconToNameConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ERHMS.Desktop.Converters
+{
+    public class IconToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            else
+            {
+                return typeof(SystemIcons).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                    .SingleOrDefault(property => property.GetValue(null) == value)
+                    ?.Name;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}

d722c1081ad81dba426cdeb9691bf5f0ecc54172
Steven Williams <gev3@cdc.gov>
2020-08-04 14:32:51 -0400

    Add throttled window size saving

--- ERHMS.Desktop/Views/MainView.xaml.cs
+++ ERHMS.Desktop/Views/MainView.xaml.cs
@@ -1,27 +1,28 @@
 ﻿using ERHMS.Desktop.Properties;
 using System;
+using System.Threading;
 using System.Windows;
 
 namespace ERHMS.Desktop.Views
 {
     public partial class MainView : Window
     {
+        private static readonly TimeSpan ResizeTimerInterval = TimeSpan.FromSeconds(1.0);
+
+        private Timer resizeTimer;
+
         public MainView()
         {
             InitializeComponent();
             Settings.Default.ApplyTo(this);
+            resizeTimer = new Timer(state => Settings.Default.Save());
         }
 
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
             base.OnRenderSizeChanged(sizeInfo);
             Settings.Default.UpdateFrom(this);
-        }
-
-        protected override void OnClosed(EventArgs e)
-        {
-            base.OnClosed(e);
-            Settings.Default.Save();
+            resizeTimer.Change(ResizeTimerInterval, Timeout.InfiniteTimeSpan);
         }
 
         private void DataContext_ExitRequested(object sender, EventArgs e)

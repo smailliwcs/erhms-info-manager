4514ea8122564a9168985c1844e1f5ec6cb767de
Steven Williams <gev3@cdc.gov>
2020-08-06 14:30:25 -0400

    Open context menu on click

---
+++ ERHMS.Desktop/Behaviors/OpenContextMenuBehavior.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace ERHMS.Desktop.Behaviors
+{
+    public class OpenContextMenuBehavior : Behavior<ButtonBase>
+    {
+        private ContextMenu ContextMenu => AssociatedObject.ContextMenu;
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewMouseDown += AssociatedObject_MouseEvent;
+            AssociatedObject.Click += AssociatedObject_MouseEvent;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseDown -= AssociatedObject_MouseEvent;
+            AssociatedObject.Click -= AssociatedObject_MouseEvent;
+        }
+
+        private void AssociatedObject_MouseEvent(object sender, RoutedEventArgs e)
+        {
+            if (ContextMenu != null)
+            {
+                ContextMenu.DataContext = AssociatedObject.DataContext;
+                ContextMenu.PlacementTarget = AssociatedObject;
+                ContextMenu.Placement = PlacementMode.Right;
+                ContextMenu.IsOpen = true;
+                e.Handled = true;
+            }
+        }
+    }
+}

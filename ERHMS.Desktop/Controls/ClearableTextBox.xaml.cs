f88675228f272a10547423321bc8860c0c643340
Steven Williams <gev3@cdc.gov>
2020-08-12 10:44:26 -0400

    Allow clearing search text

---
+++ ERHMS.Desktop/Controls/ClearableTextBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ERHMS.Desktop.Controls
+{
+    public partial class ClearableTextBox : UserControl
+    {
+        public static DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(ClearableTextBox),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Thickness padding = TextBox.Padding;
+            padding.Right += Clear.ActualWidth;
+            TextBox.Padding = padding;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Text = "";
+            GetBindingExpression(TextProperty).UpdateSource();
+        }
+    }
+}

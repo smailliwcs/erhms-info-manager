a9723bd9ac978cac2cb861a118b67eeb52725f5b
Steven Williams <gev3@cdc.gov>
2020-08-18 12:06:33 -0400

    Add infrastructure for integration

--- ERHMS.Desktop/Integration.cs
+++ ERHMS.Desktop/Integration.cs
@@ -1,21 +1,70 @@
 ﻿using ERHMS.Data;
+using ERHMS.Desktop.Dialogs;
 using ERHMS.Desktop.Properties;
+using ERHMS.Desktop.Services;
+using ERHMS.Desktop.ViewModels;
+using ERHMS.Desktop.Views;
 using ERHMS.EpiInfo.Data;
 using ERHMS.EpiInfo.Projects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace ERHMS.Desktop
 {
-    // TODO: Add logging
     public static class Integration
     {
+        private class DialogService : IDialogService
+        {
+            public bool? Show(DialogInfo info)
+            {
+                Window window = new DialogView
+                {
+                    DataContext = new DialogViewModel(info),
+                    WindowStartupLocation = WindowStartupLocation.CenterScreen
+                };
+                info.Sound?.Play();
+                return window.ShowDialog();
+            }
+        }
+
+        static Integration()
+        {
+            App.ConfigureLog();
+            ConfigureServices();
+        }
+
+        private static void ConfigureServices()
+        {
+            ServiceProvider.Install<IDialogService>(() => new DialogService());
+        }
+
+        private static void Run(Action action, [CallerMemberName] string methodName = null)
+        {
+            string title = $"{nameof(ERHMS)}.{nameof(Integration)}.{methodName}";
+            Log.Default.Debug($"Executing: {title}");
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                Log.Default.Warn(ex);
+                ServiceProvider.Resolve<IDialogService>().Show(new DialogInfo(DialogInfoPreset.Error)
+                {
+                    Lead = ResX.HandledErrorLead,
+                    Body = ex.Message,
+                    Details = ex.ToString()
+                });
+            }
+            Log.Default.Debug($"Executed: {title}");
+        }
+
         public static string LookUpWorkerId(string firstName, string lastName, string workerId)
         {
-            try
+            Run(() =>
             {
                 WorkerProject project = new WorkerProject(Settings.Default.WorkerProjectPath);
                 RecordRepository repository = new RecordRepository(project.WorkerInfoView);
@@ -28,23 +77,14 @@ namespace ERHMS.Desktop
                 IList<Record> records = repository.Select(clauses, parameters).ToList();
                 if (records.Count == 1)
                 {
-                    return records[0].GlobalRecordId;
+                    workerId = records[0].GlobalRecordId;
                 }
                 else
                 {
                     // TODO
-                    return workerId;
                 }
-            }
-            catch (Exception ex)
-            {
-                StringBuilder message = new StringBuilder();
-                message.AppendLine(ResX.HandledErrorLead);
-                message.AppendLine();
-                message.Append(ex.Message);
-                MessageBox.Show(message.ToString(), ResX.AppTitle, MessageBoxButton.OK, MessageBoxImage.Error);
-                return null;
-            }
+            });
+            return workerId;
         }
     }
 }

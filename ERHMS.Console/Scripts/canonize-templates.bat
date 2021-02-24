23e1f8df8c59e0f38bc433708b6c48c554251e8e
Steven Williams <gev3@cdc.gov>
2021-02-24 11:31:57 -0500

    Use templates from build directory

--- ERHMS.Console/Scripts/canonize-templates.bat
+++ ERHMS.Console/Scripts/canonize-templates.bat
@@ -8,8 +8,9 @@ goto :eof
 :canonize
 set project_type=%~1
 set project_path="%CD%\Projects\%project_type%\%project_type%.prj"
-for %%f in (..\..\..\ERHMS.Resources\Templates\Forms\%project_type%\*) do (
+for %%f in ("Templates\Forms\%project_type%\*") do (
     ERHMS.Console CreateTemplate "%%f" %project_path% "%%~nf"
     ERHMS.Console CanonizeTemplate "%%f"
+    copy /y "%%f" "..\..\..\ERHMS.Resources\%%f"
 )
 goto :eof
--- ERHMS.Console/Scripts/instantiate-templates.bat
+++ ERHMS.Console/Scripts/instantiate-templates.bat
@@ -13,5 +13,5 @@ set project_path="%CD%\Projects\%project_type%\%project_type%.prj"
 ERHMS.Console CreateDatabase Access2003 %connection_string%
 ERHMS.Console CreateProject Access2003 %connection_string% %project_location% %project_type%
 ERHMS.Console InitializeProject %project_path%
-for %%f in (..\..\..\ERHMS.Resources\Templates\Forms\%project_type%\*) do ERHMS.Console InstantiateTemplate "%%f" %project_path% "%%~nf"
+for %%f in ("Templates\Forms\%project_type%\*") do ERHMS.Console InstantiateTemplate "%%f" %project_path% "%%~nf"
 goto :eof

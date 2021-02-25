b95270ba13a4dad1c69bc066b834f78e5faf89ad
Steven Williams <gev3@cdc.gov>
2021-02-25 12:43:07 -0500

    Specify instantiation/canonization order

--- ERHMS.Console/Scripts/canonize-templates.bat
+++ ERHMS.Console/Scripts/canonize-templates.bat
@@ -1,16 +1,22 @@
 @echo off
 setlocal
 
-call :canonize Worker
-call :canonize Incident
+call :canonize_template Worker WorkerRosteringForm
+call :canonize_template Worker PreDeploymentHealthSurvey
+call :canonize_template Incident WorkerDeploymentRecord
+call :canonize_template Incident WorkerInProcessingForm
+call :canonize_template Incident WorkerActivityReport
+call :canonize_template Incident DeploymentHealthSurvey
+call :canonize_template Incident WorkerOutProcessingForm
+call :canonize_template Incident PostDeploymentHealthSurvey
+call :canonize_template Incident AfterActionReview
 goto :eof
 
-:canonize
+:canonize_template
 set project_type=%~1
-set project_path="%CD%\Projects\%project_type%\%project_type%.prj"
-for %%f in ("Templates\Forms\%project_type%\*") do (
-    ERHMS.Console CreateTemplate "%%f" %project_path% "%%~nf"
-    ERHMS.Console CanonizeTemplate "%%f"
-    copy /y "%%f" "..\..\..\ERHMS.Resources\%%f"
-)
+set form_name=%~2
+set template_path="Templates\Forms\%project_type%\%form_name%.xml"
+set project_path="Projects\%project_type%\%project_type%.prj"
+ERHMS.Console CreateTemplate %template_path% %project_path% %form_name%
+ERHMS.Console CanonizeTemplate %template_path%
 goto :eof
--- ERHMS.Console/Scripts/instantiate-templates.bat
+++ ERHMS.Console/Scripts/instantiate-templates.bat
@@ -1,11 +1,20 @@
 @echo off
 setlocal
 
-call :instantiate Worker
-call :instantiate Incident
+call :create_project Worker
+call :instantiate_template Worker WorkerRosteringForm
+call :instantiate_template Worker PreDeploymentHealthSurvey
+call :create_project Incident
+call :instantiate_template Incident WorkerDeploymentRecord
+call :instantiate_template Incident WorkerInProcessingForm
+call :instantiate_template Incident WorkerActivityReport
+call :instantiate_template Incident DeploymentHealthSurvey
+call :instantiate_template Incident WorkerOutProcessingForm
+call :instantiate_template Incident PostDeploymentHealthSurvey
+call :instantiate_template Incident AfterActionReview
 goto :eof
 
-:instantiate
+:create_project
 set project_type=%~1
 set connection_string="Provider=Microsoft.Jet.OLEDB.4.0;Data Source=""%CD%\Projects\%project_type%\%project_type%.mdb"""
 set project_location="%CD%\Projects\%project_type%"
@@ -13,5 +22,12 @@ set project_path="%CD%\Projects\%project_type%\%project_type%.prj"
 ERHMS.Console CreateDatabase Access2003 %connection_string%
 ERHMS.Console CreateProject Access2003 %connection_string% %project_location% %project_type%
 ERHMS.Console InitializeProject %project_path%
-for %%f in ("Templates\Forms\%project_type%\*") do ERHMS.Console InstantiateTemplate "%%f" %project_path% "%%~nf"
+goto :eof
+
+:instantiate_template
+set project_type=%~1
+set form_name=%~2
+set template_path="Templates\Forms\%project_type%\%form_name%.xml"
+set project_path="Projects\%project_type%\%project_type%.prj"
+ERHMS.Console InstantiateTemplate %template_path% %project_path% %form_name%
 goto :eof

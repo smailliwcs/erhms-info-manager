c24065e8e1e1e104b1933660ef4889bff21235eb
Steven Williams <gev3@cdc.gov>
2021-03-10 16:38:32 -0500

    Consolidate phase view models

--- ERHMS.Desktop/ViewModels/HomeViewModel.cs
+++ ERHMS.Desktop/ViewModels/HomeViewModel.cs
@@ -1,16 +1,6 @@
 ﻿using ERHMS.Desktop.Infrastructure;
-using ERHMS.Domain;
-using System.Collections.Generic;
 
 namespace ERHMS.Desktop.ViewModels
 {
-    public class HomeViewModel : ObservableObject
-    {
-        public IReadOnlyCollection<PhaseViewModel> Phases { get; } = new PhaseViewModel[]
-        {
-            new PhaseViewModel(Phase.PreDeployment),
-            new PhaseViewModel(Phase.Deployment),
-            new PhaseViewModel(Phase.PostDeployment)
-        };
-    }
+    public class HomeViewModel : ObservableObject { }
 }
--- ERHMS.Desktop/ViewModels/PhaseViewModel.cs
+++ ERHMS.Desktop/ViewModels/PhaseViewModel.cs
@@ -1,17 +1,31 @@
 ﻿using ERHMS.Domain;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ERHMS.Desktop.ViewModels
 {
     public class PhaseViewModel
     {
-        public Phase Value { get; }
-        public CoreProject CoreProject => Value.ToCoreProject();
-        public IEnumerable<CoreView> CoreViews => CoreView.GetInstances(Value);
+        public static PhaseViewModel PreDeployment { get; } = new PhaseViewModel(Phase.PreDeployment);
+        public static PhaseViewModel Deployment { get; } = new PhaseViewModel(Phase.Deployment);
+        public static PhaseViewModel PostDeployment { get; } = new PhaseViewModel(Phase.PostDeployment);
 
-        public PhaseViewModel(Phase value)
+        public static IReadOnlyCollection<PhaseViewModel> Instances { get; } = new PhaseViewModel[]
+        {
+            PreDeployment,
+            Deployment,
+            PostDeployment
+        };
+
+        public Phase Value { get; }
+        public CoreProject CoreProject { get; }
+        public IReadOnlyCollection<CoreView> CoreViews { get; }
+
+        private PhaseViewModel(Phase value)
         {
             Value = value;
+            CoreProject = value.ToCoreProject();
+            CoreViews = CoreView.GetInstances(value).ToList();
         }
     }
 }

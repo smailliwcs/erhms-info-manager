c18f17f6edb6ca465fc319d61f4ec3ce111a0830
Steven Williams <gev3@cdc.gov>
2021-03-11 21:45:57 -0500

    Rework phase view models

---
+++ ERHMS.Desktop/ViewModels/Collections/PhaseCollectionViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ERHMS.Desktop.Data;
+using ERHMS.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ERHMS.Desktop.ViewModels.Collections
+{
+    public class PhaseCollectionViewModel : ViewModel
+    {
+        public class ItemViewModel : ViewModel, ISelectable
+        {
+            public Phase Value { get; }
+            public CoreProject CoreProject { get; }
+            public IReadOnlyCollection<CoreView> CoreViews { get; }
+
+            private bool selected;
+            public bool Selected
+            {
+                get { return selected; }
+                set { SetProperty(ref selected, value); }
+            }
+
+            public ItemViewModel(Phase value)
+            {
+                Value = value;
+                CoreProject = value.ToCoreProject();
+                CoreViews = CoreView.GetInstances(value).ToList();
+            }
+
+            public override int GetHashCode()
+            {
+                return Value.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is ItemViewModel item && Value == item.Value;
+            }
+        }
+
+        private readonly List<ItemViewModel> items;
+        public CustomCollectionView<ItemViewModel> Items { get; }
+
+        public PhaseCollectionViewModel()
+        {
+            items = new List<ItemViewModel>
+            {
+                new ItemViewModel(Phase.PreDeployment),
+                new ItemViewModel(Phase.Deployment),
+                new ItemViewModel(Phase.PostDeployment)
+            };
+            Items = new CustomCollectionView<ItemViewModel>(items);
+        }
+    }
+}
--- ERHMS.Desktop/ViewModels/HomeViewModel.cs
+++ ERHMS.Desktop/ViewModels/HomeViewModel.cs
@@ -1,4 +1,14 @@
-﻿namespace ERHMS.Desktop.ViewModels
+﻿using ERHMS.Desktop.ViewModels.Collections;
+
+namespace ERHMS.Desktop.ViewModels
 {
-    public class HomeViewModel : ViewModel { }
+    public class HomeViewModel : ViewModel
+    {
+        public PhaseCollectionViewModel Phases { get; }
+
+        public HomeViewModel()
+        {
+            Phases = new PhaseCollectionViewModel();
+        }
+    }
 }

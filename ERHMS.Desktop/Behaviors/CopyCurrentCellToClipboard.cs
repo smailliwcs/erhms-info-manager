5bdcdb78cb254d1d8e9c25e7d5946d6f84d41ab6
Steven Williams <gev3@cdc.gov>
2021-03-17 11:05:00 -0400

    Allow copying current cell to clipboard

---
+++ ERHMS.Desktop/Behaviors/CopyCurrentCellToClipboard.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace ERHMS.Desktop.Behaviors
+{
+    public class CopyCurrentCellToClipboard : Behavior<DataGrid>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.CopyingRowClipboardContent += AssociatedObject_CopyingRowClipboardContent;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.CopyingRowClipboardContent -= AssociatedObject_CopyingRowClipboardContent;
+        }
+
+        private void AssociatedObject_CopyingRowClipboardContent(object sender, DataGridRowClipboardEventArgs e)
+        {
+            DataGridClipboardCellContent currentCellContent = e.ClipboardRowContent
+                .SingleOrDefault(content => content.Column.Equals(AssociatedObject.CurrentColumn));
+            if (currentCellContent != null)
+            {
+                e.ClipboardRowContent.Clear();
+                e.ClipboardRowContent.Add(currentCellContent);
+            }
+        }
+    }
+}

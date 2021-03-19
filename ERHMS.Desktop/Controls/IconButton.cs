0f14af81aa7638a3805f92c43bf7f31b577e2fdf
Steven Williams <gev3@cdc.gov>
2021-03-19 10:09:11 -0400

    Add access keys for icon buttons

--- ERHMS.Desktop/Controls/IconButton.cs
+++ ERHMS.Desktop/Controls/IconButton.cs
@@ -1,10 +1,17 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ERHMS.Desktop.Controls
 {
     public class IconButton : Button
     {
+        public static readonly DependencyProperty AccessKeyProperty = DependencyProperty.Register(
+            nameof(AccessKey),
+            typeof(string),
+            typeof(IconButton),
+            new FrameworkPropertyMetadata(AccessKeyProperty_DependencyPropertyChanged));
+
         public static readonly DependencyProperty AltTextProperty = DependencyProperty.Register(
             nameof(AltText),
             typeof(string),
@@ -16,6 +23,27 @@ namespace ERHMS.Desktop.Controls
                 typeof(IconButton),
                 new FrameworkPropertyMetadata(typeof(IconButton)));
             IsTabStopProperty.OverrideMetadata(typeof(IconButton), new FrameworkPropertyMetadata(false));
+        }
+
+        private static void AccessKeyProperty_DependencyPropertyChanged(
+            DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            IInputElement element = (IInputElement)sender;
+            if (e.OldValue is string oldKey)
+            {
+                AccessKeyManager.Unregister(oldKey, element);
+            }
+            if (e.NewValue is string newKey)
+            {
+                AccessKeyManager.Register(newKey, element);
+            }
+        }
+
+        public string AccessKey
+        {
+            get { return (string)GetValue(AccessKeyProperty); }
+            set { SetValue(AccessKeyProperty, value); }
         }
 
         public string AltText

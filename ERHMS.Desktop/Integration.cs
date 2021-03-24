f9b4b2f6b50d1e51fa4de529c36f163bc321726c
Steven Williams <gev3@cdc.gov>
2021-03-24 12:04:06 -0400

    Stub out worker ID lookup

---
+++ ERHMS.Desktop/Integration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ERHMS.Desktop
+{
+    public static class Integration
+    {
+        public static string WorkerInfo_GetGlobalRecordId(
+            string firstName,
+            string lastName,
+            string emailAddress,
+            string globalRecordId)
+        {
+            return Guid.Empty.ToString();
+        }
+    }
+}

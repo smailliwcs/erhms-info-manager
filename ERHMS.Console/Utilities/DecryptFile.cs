83eed57b5f311e11fafe2181d8ed92d50049522a
Steven Williams <gev3@cdc.gov>
2021-06-09 15:07:17 -0400

    Add file encryption/decryption utilities

---
+++ ERHMS.Console/Utilities/DecryptFile.cs
@@ -0,0 +1,24 @@
+﻿namespace ERHMS.Console.Utilities
+{
+    public class DecryptFile : IUtility
+    {
+        public string InputPath { get; }
+        public string OutputPath { get; }
+        public string Password { get; }
+
+        public DecryptFile(string inputPath, string outputPath, string password)
+        {
+            InputPath = inputPath;
+            OutputPath = outputPath;
+            Password = password;
+        }
+
+        public DecryptFile(string inputPath, string outputPath)
+            : this(inputPath, outputPath, "") { }
+
+        public void Run()
+        {
+            Epi.Configuration.DecryptFile(InputPath, OutputPath, Password);
+        }
+    }
+}
---
+++ ERHMS.Console/Utilities/EncryptFile.cs
@@ -0,0 +1,24 @@
+﻿namespace ERHMS.Console.Utilities
+{
+    public class EncryptFile : IUtility
+    {
+        public string InputPath { get; }
+        public string OutputPath { get; }
+        public string Password { get; }
+
+        public EncryptFile(string inputPath, string outputPath, string password)
+        {
+            InputPath = inputPath;
+            OutputPath = outputPath;
+            Password = password;
+        }
+
+        public EncryptFile(string inputPath, string outputPath)
+            : this(inputPath, outputPath, "") { }
+
+        public void Run()
+        {
+            Epi.Configuration.EncryptFile(InputPath, OutputPath, Password);
+        }
+    }
+}

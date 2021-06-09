34fdc4a50227b3a869d6b1bb973f5769415bfca4
Steven Williams <gev3@cdc.gov>
2021-06-09 15:07:43 -0400

    Add data unpackaging utility

---
+++ ERHMS.Console/Utilities/UnpackageData.cs
@@ -0,0 +1,36 @@
+﻿using Epi.ImportExport;
+using System;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace ERHMS.Console.Utilities
+{
+    public class UnpackageData : IUtility
+    {
+        private static readonly string sentinel = "[[EPIINFO7_DATAPACKAGE]]";
+
+        public string DataPath { get; }
+        public string Password { get; }
+
+        public UnpackageData(string dataPath, string password)
+        {
+            DataPath = dataPath;
+            Password = password;
+        }
+
+        public UnpackageData(string dataPath)
+            : this(dataPath, "") { }
+
+        public void Run()
+        {
+            string content = Epi.Configuration.DecryptFileToString(DataPath, Password);
+            if (!content.StartsWith(sentinel))
+            {
+                throw new InvalidOperationException("File does not appear to be an Epi Info data package.");
+            }
+            string xml = ImportExportHelper.UnZip(content.Substring(sentinel.Length));
+            XDocument document = XDocument.Parse(xml);
+            Out.WriteLine(document);
+        }
+    }
+}

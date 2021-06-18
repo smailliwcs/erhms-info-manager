1b6779505646d9fa2de0ec28c3722feec3a90c49
Steven Williams <gev3@cdc.gov>
2021-06-18 09:21:32 -0400

    Add field listing utility

---
+++ ERHMS.Console/Utilities/ListFields.cs
@@ -0,0 +1,39 @@
+﻿using Epi;
+using Epi.Data.Services;
+using ERHMS.EpiInfo;
+using ERHMS.EpiInfo.Metadata;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace ERHMS.Console.Utilities
+{
+    public class ListFields : IUtility
+    {
+        public string ProjectPath { get; }
+
+        public ListFields(string projectPath)
+        {
+            ProjectPath = projectPath;
+        }
+
+        public void Run()
+        {
+            Project project = ProjectExtensions.Open(ProjectPath);
+            foreach (View view in project.Views)
+            {
+                Out.WriteLine($"{view.Name}");
+                foreach (Page page in view.Pages)
+                {
+                    Out.WriteLine($"  {page.Name}");
+                    FieldDataTable fields = ((MetadataDbProvider)project.Metadata).GetFieldDataTableForPage(page.Id);
+                    IComparer<FieldDataRow> comparer = new FieldDataRowComparer.ByEffectiveTabIndex(fields);
+                    foreach (FieldDataRow field in fields.OrderBy(field => field, comparer))
+                    {
+                        Out.WriteLine($"    {field.Name}");
+                    }
+                }
+            }
+        }
+    }
+}

3355db06c94101007761b1061d824c4533f09f92
Steven Williams <gev3@cdc.gov>
2021-06-18 10:57:13 -0400

    Add missing file

---
+++ ERHMS.Desktop/Markup/IconExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace ERHMS.Desktop.Markup
+{
+    [MarkupExtensionReturnType(typeof(string))]
+    public class IconExtension : MarkupExtension
+    {
+        [ConstructorArgument("name")]
+        public string Name { get; set; }
+
+        public IconExtension() { }
+
+        public IconExtension(string name)
+            : this()
+        {
+            Name = name;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Icons.GetInstance(Name);
+        }
+    }
+}

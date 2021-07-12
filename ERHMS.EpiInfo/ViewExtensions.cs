cb4d4dd35533ebf3e674ac30f299ff93075ac59f
Steven Williams <gev3@cdc.gov>
2021-07-12 18:42:32 -0400

    Add .edp7 import/export

--- ERHMS.EpiInfo/ViewExtensions.cs
+++ ERHMS.EpiInfo/ViewExtensions.cs
@@ -1,11 +1,14 @@
 ﻿using Epi;
 using Epi.Data.Services;
 using Epi.Fields;
+using Epi.Windows.ImportExport.Dialogs;
 using ERHMS.Common.Logging;
 using ERHMS.EpiInfo.Naming;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Windows.Forms;
+using View = Epi.View;
 
 namespace ERHMS.EpiInfo
 {
@@ -14,6 +17,24 @@ namespace ERHMS.EpiInfo
         public static Page GetPageByName(this View @this, string pageName)
         {
             return @this.Pages.SingleOrDefault(page => NameComparer.Default.Equals(page.Name, pageName));
+        }
+
+        public static bool ImportFromPackage(this View @this)
+        {
+            ImportEncryptedDataPackageDialog dialog = new ImportEncryptedDataPackageDialog(@this)
+            {
+                StartPosition = FormStartPosition.CenterParent
+            };
+            return dialog.ShowDialog() == DialogResult.OK;
+        }
+
+        public static void ExportToPackage(this View @this)
+        {
+            PackageForTransportDialog dialog = new PackageForTransportDialog(@this.Project.FilePath, @this)
+            {
+                StartPosition = FormStartPosition.CenterParent
+            };
+            dialog.ShowDialog();
         }
 
         public static void Unrelate(this View @this)

df993ccbb934c68936d3933da04c59dfefc09e9e
Steven Williams <gev3@cdc.gov>
2021-08-09 16:28:30 -0400

    Add access keys for icon buttons

--- ERHMS.Desktop/Controls/IconButton.cs
+++ ERHMS.Desktop/Controls/IconButton.cs
@@ -1,11 +1,17 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ERHMS.Desktop.Controls
 {
-    // TODO: Allow access key
     public class IconButton : Button
     {
+        public static readonly DependencyProperty AccessKeyProperty = DependencyProperty.Register(
+            nameof(AccessKey),
+            typeof(string),
+            typeof(IconButton),
+            new FrameworkPropertyMetadata(AccessKey_PropertyChanged));
+
         public static readonly DependencyProperty AltTextProperty = DependencyProperty.Register(
             nameof(AltText),
             typeof(string),
@@ -15,6 +21,25 @@ namespace ERHMS.Desktop.Controls
             nameof(HelpText),
             typeof(string),
             typeof(IconButton));
+
+        private static void AccessKey_PropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            IconButton button = (IconButton)sender;
+            if (e.OldValue is string oldKey)
+            {
+                AccessKeyManager.Unregister(oldKey, button);
+            }
+            if (e.NewValue is string newKey)
+            {
+                AccessKeyManager.Register(newKey, button);
+            }
+        }
+
+        public string AccessKey
+        {
+            get { return (string)GetValue(AccessKeyProperty); }
+            set { SetValue(AccessKeyProperty, value); }
+        }
 
         public string AltText
         {

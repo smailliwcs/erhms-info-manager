6ff3019af36896adc06be5348d6f9f3120f10eee
Steven Williams <gev3@cdc.gov>
2021-08-11 10:00:05 -0400

    Stub out help view

---
+++ ERHMS.Desktop/Controls/WebHyperlink.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace ERHMS.Desktop.Controls
+{
+    public class WebHyperlink : Hyperlink
+    {
+        public WebHyperlink()
+        {
+            RequestNavigate += OnRequestNavigate;
+        }
+
+        private void OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.ToString())?.Dispose();
+        }
+    }
+}
---
+++ ERHMS.Desktop/Views/HelpView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ERHMS.Desktop.Views
+{
+    public partial class HelpView : Window
+    {
+        public HelpView()
+        {
+            InitializeComponent();
+        }
+    }
+}

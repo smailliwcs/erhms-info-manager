66e14d12ab09b75ae12cedbf7ac7d90fc4b8368a
Steven Williams <gev3@cdc.gov>
2021-08-11 04:27:14 -0400

    Add core view creation utility

---
+++ ERHMS.Console/Utilities/CreateCoreViews.cs
@@ -0,0 +1,35 @@
+﻿using Epi;
+using ERHMS.Common.Logging;
+using ERHMS.Domain;
+using ERHMS.EpiInfo;
+using ERHMS.EpiInfo.Templating;
+using ERHMS.Resources;
+
+namespace ERHMS.Console.Utilities
+{
+    public class CreateCoreViews : IUtility
+    {
+        public string ProjectPath { get; }
+        public CoreProject CoreProject { get; }
+
+        public CreateCoreViews(string projectPath, CoreProject coreProject)
+        {
+            ProjectPath = projectPath;
+            CoreProject = coreProject;
+        }
+
+        public void Run()
+        {
+            Project project = ProjectExtensions.Open(ProjectPath);
+            foreach (CoreView coreView in CoreView.GetInstances(CoreProject))
+            {
+                ViewTemplateInstantiator instantiator =
+                    new ViewTemplateInstantiator(ResourceManager.GetXTemplate(coreView), project)
+                    {
+                        Progress = Log.Progress
+                    };
+                instantiator.Instantiate();
+            }
+        }
+    }
+}

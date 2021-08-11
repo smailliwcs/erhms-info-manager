9d91150d5e832b442db6e75ff99f6f95a191d926
Steven Williams <gev3@cdc.gov>
2021-08-11 04:27:29 -0400

    Continue deletion script on error

--- ERHMS.Console/Scripts/delete.bat
+++ ERHMS.Console/Scripts/delete.bat
@@ -2,15 +2,15 @@
 setlocal
 
 :main
-call :delete_project Blank || exit /b 1
-call :delete_project Workers || exit /b 1
-call :delete_project Incident || exit /b 1
+call :delete_project Blank
+call :delete_project Workers
+call :delete_project Incident
 goto :eof
 
 :delete_project
 set project_location=Projects\%~1
 set project_name=%~n1
 set project_path=%project_location%\%project_name%.prj
-ERHMS.Console DeleteDatabase "%project_path%" || exit /b 1
-ERHMS.Console DeleteProject "%project_path%" true || exit /b 1
+ERHMS.Console DeleteDatabase "%project_path%"
+ERHMS.Console DeleteProject "%project_path%" true
 goto :eof

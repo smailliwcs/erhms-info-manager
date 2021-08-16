dc26f38b868fb74db2d16933ff4a022b76f14bc4
Steven Williams <gev3@cdc.gov>
2021-08-16 16:33:04 -0400

    Reset active tab when navigating

---
+++ ERHMS.Desktop/Behaviors/ResetSelectedIndexOnDataContextChanged.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ERHMS.Desktop.Behaviors
+{
+    public class ResetSelectedIndexOnDataContextChanged : Behavior<Selector>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.DataContextChanged += AssociatedObject_DataContextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.DataContextChanged -= AssociatedObject_DataContextChanged;
+        }
+
+        private void AssociatedObject_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (AssociatedObject.HasItems)
+            {
+                AssociatedObject.SelectedIndex = 0;
+            }
+        }
+    }
+}

b22f11ab30c0b4b2846573471c8cf5133f225841
Steven Williams <gev3@cdc.gov>
2021-09-03 12:48:54 -0400

    Bump version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ERHMS Info Manager")]
 [assembly: AssemblyCompany("NIOSH")]
 [assembly: AssemblyVersion("3.0")]
-[assembly: AssemblyInformationalVersion("3.0.0 RC1 (September 1, 2021)")]
+[assembly: AssemblyInformationalVersion("3.0.0 RC1 (September 3, 2021)")]
 [assembly: AssemblyCopyright("\u00a9 2021 National Institute for Occupational Safety and Health")]
 [assembly: ComVisible(false)]

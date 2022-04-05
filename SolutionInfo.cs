abe61107a36a0503309eb52673e2691cd98d1439
Steven Williams <gev3@cdc.gov>
2022-04-05 10:30:31 -0400

    Update copyright year

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("NIOSH")]
 [assembly: AssemblyVersion("3.0.0.2")]
 [assembly: AssemblyInformationalVersion("3.0 RC2")]
-[assembly: AssemblyCopyright("\u00a9 2021 National Institute for Occupational Safety and Health")]
+[assembly: AssemblyCopyright("\u00a9 2022 National Institute for Occupational Safety and Health")]
 [assembly: ComVisible(false)]
